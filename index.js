0516f1dcbe76b408556d6f12099f5b4c231975a0
truegrit <7505695+grittrue@user.noreply.gitee.com>
2020-09-02 17:10:17 +0800

    node1

---
+++ index.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+const http = require('http')
+const server = htt.createServer()
+server.on('request',function(){
+    console.log('我是requese')
+})
+
+server.listen(3000,function(){
+    console.log("启动")
+})
