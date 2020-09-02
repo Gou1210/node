b0a55f3afe61ff3d9adb4b244211afebc797a974
truegrit <7505695+grittrue@user.noreply.gitee.com>
2020-09-02 17:32:57 +0800

    2020年9月2日17.32

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const fs = require('fs')
 const http = require('http')
 const server = http.createServer()
-server.on('request',function(){
-    console.log('我是requese')
+server.on('request',function(request,response){
+    console.log('收到客户端请求'+request.url)
+    response.wridte('hello')
+    response.end()
 })
 
 server.listen(3000,function(){
